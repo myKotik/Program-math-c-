1bf8e1abc16fc2b0d236cbfa9fce4fbde6976f5b
myKotik <Lebedeva.kate6518@gmail.com>
2021-06-27 15:54:38 +0200

    Добавьте файлы проекта.

---
+++ three/three.cpp
@@ -0,0 +1,20 @@
+﻿// комментарий
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char* argv[]) {
+    setlocale(LC_ALL, "RUS");//правильно отображать русский язык
+    int a, b, c, x;
+    cout << "Введите число a, b и c" << endl;
+    cout << "a=";
+    cin >> a;
+    cout << "b=";
+    cin >> b;
+    cout << "c=";
+    cin >> c;
+    cout << "Вичисляем по формуле (а + b — f / а) + f * a * a — (a + b)" << endl;
+    x = (a + b - c / a) + c * a * a - (a + b);
+    cout << "x=" << x;
+}
